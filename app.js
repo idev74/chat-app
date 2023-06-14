596c2e50097661b5b7059563654436b378742a1c
Isabella Devassy <77900249+idev74@users.noreply.github.com>
2023-06-14 14:49:06 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+
+//Socket.io
+const io = require('socket.io')(server);
+io.on("connection", (socket) => {
+  console.log("🔌 New user connected! 🔌");
+})
+
+const exphbs  = require('express-handlebars');
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => {
+  res.render('index.handlebars');
+})
+
+server.listen('3000', () => {
+  console.log('Server listening on Port 3000');
+})
