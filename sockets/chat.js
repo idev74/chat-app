f67fc49b6621278b7127ba5a2f2071d48a69f878
Isabella Devassy <77900249+idev74@users.noreply.github.com>
2023-06-21 17:49:03 -0700

    Users can join chat and send messages

--- sockets/chat.js
+++ sockets/chat.js
@@ -1,7 +1,11 @@
 module.exports = (io, socket) => {
-    // Listen for "new user" socket emits
     socket.on('new user', (username) => {
         console.log(`${username} has joined the chat! ✋`);
         io.emit("new user", username);
+    })
+
+    socket.on('new message', (data) => {
+        console.log(`🎤 ${data.sender}: ${data.message} 🎤`)
+        io.emit('new message', data);
     })
 }
