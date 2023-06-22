a992eaca17e2c832e6d0d361423d38fe130d5207
Isabella Devassy <77900249+idev74@users.noreply.github.com>
2023-06-21 17:40:53 -0700

    Users can log in

--- public/index.js
+++ public/index.js
@@ -1,4 +1,16 @@
-$(document).ready( () => {
-    //Connect to the socket.io server
+$(document).ready(() => {
     const socket = io.connect();
-  })
+    $('#create-user-btn').click((e) => {
+        e.preventDefault();
+        let username = $('#username-input').val();
+        if (username.length > 0) {
+            //Emit to the server the new user
+            socket.emit('new user', username);
+            $('.username-form').remove();
+        }
+    });
+
+    socket.on('new user', (username) => {
+        console.log(`✋ ${username} has joined the chat! ✋`);
+    })
+})
